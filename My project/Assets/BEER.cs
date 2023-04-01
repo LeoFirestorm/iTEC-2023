3ce5edc538d143178a9dbca6797abc63819c090b
Mihai <mihaigutovschi@gmail.com>
2023-04-01 16:52:25 +0300

    Revert "Auto stash before merge of "main" and "origin/main""
    This reverts commit ba4ea69eddaa17ea37faa55e44424d71106e0149.

--- My project/Assets/BEER.cs
+++ My project/Assets/BEER.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class beer : MonoBehaviour
+public class Beer : MonoBehaviour
 {
-    
-        private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerInventory PlayerInventory = other.GetComponent<PlayerInventory>();
+        if (PlayerInventory != null)
         {
-            PlayerInventory PlayerInventory = other.GetComponent<PlayerInventory>();
-            if (PlayerInventory != null)
-            {
-                PlayerInventory.BeerCollected();
-                gameObject.SetActive(false);
-            }
+            PlayerInventory.BeerCollected();
+            gameObject.SetActive(false);
         }
-    
+    }
 }
